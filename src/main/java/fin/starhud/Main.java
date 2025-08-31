62fcbf907d8683dd178133dc44c7762a9548b56e
shoker537 <andreybest2002@ya.ru>
2025-08-31 13:30:44 +0300

    Reset money data after world change to only show money hud on supported worlds/servers

--- src/main/java/fin/starhud/Main.java
+++ src/main/java/fin/starhud/Main.java
@@ -6,10 +6,10 @@ import fin.starhud.network.MoneyDataPayload;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientWorldEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents;
-import net.minecraft.client.world.ClientWorld;
 
 public class Main implements ClientModInitializer {
 
@@ -19,8 +19,11 @@ public class Main implements ClientModInitializer {
     public void onInitializeClient() {
         AutoConfig.register(Settings.class, GsonConfigSerializer::new);
         settings = AutoConfig.getConfigHolder(Settings.class).getConfig();
-
         ServerLoginConnectionEvents.INIT.register((serverLoginNetworkHandler, minecraftServer) -> {
+            Money.privateValue = null;
+            Money.teamValue = null;
+        });
+        ClientWorldEvents.AFTER_CLIENT_WORLD_CHANGE.register((minecraftClient, clientWorld) -> {
             Money.privateValue = null;
             Money.teamValue = null;
         });
